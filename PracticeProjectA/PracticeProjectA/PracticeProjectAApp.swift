b7ee5afd6b99c0f46e16e1396ca62578445c8df6
Dsimpson123 <darren.simpson@hgs.hiddengeniusproject.org>
2026-06-16 13:15:28 -0700

    uploaded practice project a

---
+++ PracticeProjectA/PracticeProjectA/PracticeProjectAApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeProjectAApp.swift
+//  PracticeProjectA
+//
+//  Created by Darren Simpson on 6/16/26.
+//
+
+import SwiftUI
+
+@main
+struct PracticeProjectAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
